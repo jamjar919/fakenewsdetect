039a114491e9eb1a18fb77334dc562311b33dabe
James Paterson <james@mjsquare.com>
2018-02-20 23:02:56 +0000

    iijo

--- index.py
+++ index.py
@@ -4,9 +4,14 @@ import nltk
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
-#nltk.download();
+from functools import reduce
 
-def loadArticles(name)
+
+# Constants
+NUMBER_OF_TEST_ARTICLES = 100;
+VOCAB_SIZE = 1000;
+
+def loadArticles(name):
     return pd.read_csv(name, header=0, delimiter=",")
 
 def cleanText(dirty):
@@ -17,7 +22,7 @@ def cleanText(dirty):
     clean = [w for w in clean if not w in stops]
     return (" ".join(clean));
 
-def calculateTermFrequency(corpus, max_features=None, ngrams=1):
+def calculateTermFrequency(corpus, max_features=None, ngrams=1, vocabulary=None):
     '''
     Returns a list of terms and their count given a list of strings
     '''
@@ -27,7 +32,8 @@ def calculateTermFrequency(corpus, max_features=None, ngrams=1):
         preprocessor = None,
         stop_words = None,
         max_features = max_features,
-        ngram_range=(ngrams,ngrams)
+        ngram_range=(ngrams,ngrams),
+        vocabulary=vocabulary
     );
     features = vectorizer.fit_transform(corpus);
     return (features.toarray(), vectorizer.get_feature_names());
@@ -37,55 +43,119 @@ def printFrequencyCount(frequency, terms):
     for tag, count in zip(terms, dist):
         print(count, tag)
 
-def probabilityOfWordGivenClass(word, frequencies, totalFrequencies, totalVocab):
+def probabilityOfWordGivenClass(word, frequencies, vocab, totalFrequencies, totalVocab):
     '''
     We use laplace smoothing here
     '''
+    print("TOTAL FREQ",totalFrequencies,"TOTAL VOCAB",totalVocab)
     # Find word in frequencies list
+    try:
+        print("Looking for frequency of",word, end=" ")
+        index = vocab.index(word);
+        freq = frequencies[index];
+        print("Frequency is",freq, end=" ")
+        prob = (freq + 1)/(totalFrequencies + totalVocab);
+        print("Prob is ",prob)
+        return prob;
+    except ValueError:
+        print("DID NOT FIND IN VOCAB")
+        return 1;
 
+def probabilityOfClassGivenDocument(document, c, totalFrequencies, totalVocab):
+    frequencies, terms, P_c, _ = c;
+    # Bayes rule for class c and document d - P(c|d) = P(d|c)P(c)/P(d)
+    # We can get our class mapping by representing the document d as a set of features x1,x2...xn
+    # Hence calculate P(x1|c)P(x2|c)...P(xn|c)
+
+    print(totalVocab)
+    
+    # Extract words from our document
+    features = text_to_word_sequence(document);
+    probabilities = list();
+    for f in features:
+        probabilities.append(probabilityOfWordGivenClass(f, frequencies, terms, totalFrequencies, totalVocab));
+    pi = np.float64(1);
+    for p in probabilities:
+        pi = pi * np.float64(p);
+    return pi*np.float64(P_c);
+
+def documentClass(document, classes = []):
+    # Classes = An array of (frequencies, terms, P_c, name)
+    # Calculate vocab sizes and total the frequencies for each class
+    vocabSizes = list();
+    totalFrequencies = list();
+    for c in classes:
+        vocabSizes.append(len(c[1]));
+        print(len(c[1]))
+        print(len(c[0]))
+        totalFrequencies.append(np.sum(c[0]));
+    # Match the document to each class:
+    print("Testing document");
+    print(document[0:300]);
+    for i in range(0, len(classes)):
+        c = classes[i];
+        prob = probabilityOfClassGivenDocument(cleanText(document), c, totalFrequencies[i], vocabSizes[i]);
+        print("Document has a "+str(prob)+" chance of being in class "+c[3]);
 
 articles = loadArticles("news_ds.csv")
+
+# Split into training articles and test articles
+test = articles[0:NUMBER_OF_TEST_ARTICLES];
+articles = articles[NUMBER_OF_TEST_ARTICLES:];
+
+print(test);
+print(articles);
 
 # Calculate base probabilities for P_fake, P_real (the chance of an article being classified into these categories)
 numArticles = articles["TEXT"].size
 
 fakeArticles = list();
 realArticles = list();
+allArticles = list();
 
 # Tokenize the document - this converts to lowercase and removes punctuation. Do this for all fake and all real articles
 for i in range(0, numArticles):
-    if articles["LABEL"][i] == 0:
-        fakeArticles.append(cleanText(articles["TEXT"][i]));
+    clean = cleanText(articles["TEXT"].iloc[i]);
+    allArticles.append(clean);
+    if articles["LABEL"].iloc[i] == 0:
+        fakeArticles.append(clean);
     else:
-        realArticles.append(cleanText(articles["TEXT"][i]));
+        realArticles.append(clean);
 
 numReal = len(realArticles);
 numFake = len(fakeArticles);
 
 print("Tokenised",numReal,"real articles,",numFake,"fake",numArticles,"total");
 
+print("Extracting vocabulary")
+frequency, terms = calculateTermFrequency(allArticles, max_features=VOCAB_SIZE)
+print("vocab")
+print(terms)
+
 # Calculate P(c)'s
 P_fake = numFake / numArticles;
 P_real = numReal / numArticles;
 
+print("P_fake:",P_fake,"  P_real:",P_real);
+
 # Calculate ngrams
 # Unigrams
-frequencyReal, termsReal = calculateTermFrequency(realArticles);
-frequencyFake, termsFake = calculateTermFrequency(fakeArticles);
+frequencyReal, termsReal = calculateTermFrequency(realArticles, max_features=VOCAB_SIZE, vocabulary=terms);
+frequencyFake, termsFake = calculateTermFrequency(fakeArticles, max_features=VOCAB_SIZE, vocabulary=terms);
 
-# Vocab sizes
-vocabReal = len(termsReal);
-vocabFake = len(termsFake);
+# Calculate total frequencies
+frequencySumReal = np.sum(frequencyReal, axis=0)
+frequencySumFake = np.sum(frequencyFake, axis=0)
 
-# Total count of the frequencies
-totalFrequenciesReal = np.sum(np.sum(frequencyReal, axis=0));
-totalFrequenciesFake = np.sum(np.sum(frequencyFake, axis=0));
+# Set up evaluation vars
+numCorrectPositive = 0;
+numFalsePositive = 0;
+numFalseNegative = 0;
+numCorrectNegative = 0;
 
-#printFrequencyCount(frequency, terms);
-
-# Bayes rule for class c and document d - P(c|d) = P(d|c)P(c)/P(d)
-# We can get our class mapping by representing the document d as a set of features x1,x2...xn
-# And finding the class c s.t P(x1,x2...xn|c)P(c) is maximised
-# Hence calculate P(x1|c)P(x2|c)...P(xn|c) for real and fake classes for our documents
-
-
+# Test with first 5 fake documents and first 5 real
+for i in range(0, 3):#test["TEXT"].size):
+    # Test real one 
+    document = test["TEXT"].iloc[i];
+    c = documentClass(document, [(frequencySumFake, termsFake, P_fake, "Fake"), (frequencySumReal, termsReal, P_real, "Real")])
+    
