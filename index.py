54c76bd27cca27d08eb6be863a10c4d95360192e
James Paterson <james@mjsquare.com>
2018-02-20 14:48:28 +0000

    cleaning and other things

--- index.py
+++ index.py
@@ -1,52 +1,91 @@
 import pandas as pd 
 from keras.preprocessing.text import text_to_word_sequence
+import nltk
 from nltk.corpus import stopwords
-nltk.download();
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+#nltk.download();
 
+def loadArticles(name)
+    return pd.read_csv(name, header=0, delimiter=",")
 
 def cleanText(dirty):
     # Convert to lowercase and remove punctuation
-    clean = text_to_word_sequence(article[1]);
+    clean = text_to_word_sequence(dirty);
     # Remove stopwords
     stops = set(stopwords.words("english"));
     clean = [w for w in clean if not w in stops]
     return (" ".join(clean));
 
+def calculateTermFrequency(corpus, max_features=None, ngrams=1):
+    '''
+    Returns a list of terms and their count given a list of strings
+    '''
+    vectorizer = CountVectorizer(
+        analyzer = "word",
+        tokenizer = None,
+        preprocessor = None,
+        stop_words = None,
+        max_features = max_features,
+        ngram_range=(ngrams,ngrams)
+    );
+    features = vectorizer.fit_transform(corpus);
+    return (features.toarray(), vectorizer.get_feature_names());
 
-articles = pd.read_csv("news_ds.csv", header=0, delimiter=",")
+def printFrequencyCount(frequency, terms):
+    dist = np.sum(frequency, axis=0)
+    for tag, count in zip(terms, dist):
+        print(count, tag)
 
-print(articles)
+def probabilityOfWordGivenClass(word, frequencies, totalFrequencies, totalVocab):
+    '''
+    We use laplace smoothing here
+    '''
+    # Find word in frequencies list
+
+
+articles = loadArticles("news_ds.csv")
 
 # Calculate base probabilities for P_fake, P_real (the chance of an article being classified into these categories)
-numArticles = len(articles);
-numFake = 0;
-numReal = 0;
+numArticles = articles["TEXT"].size
 
 fakeArticles = list();
 realArticles = list();
 
+# Tokenize the document - this converts to lowercase and removes punctuation. Do this for all fake and all real articles
 for i in range(0, numArticles):
-    if (articles["TEXT"][i] == '1'):
-        realArticles.append(article);
-        numReal += 1;
+    if articles["LABEL"][i] == 0:
+        fakeArticles.append(cleanText(articles["TEXT"][i]));
     else:
-        fakeArticles.append(article);
-        numFake += 1;
+        realArticles.append(cleanText(articles["TEXT"][i]));
 
+numReal = len(realArticles);
+numFake = len(fakeArticles);
+
+print("Tokenised",numReal,"real articles,",numFake,"fake",numArticles,"total");
+
+# Calculate P(c)'s
 P_fake = numFake / numArticles;
 P_real = numReal / numArticles;
 
-# Preprocess text in the articles using keras
+# Calculate ngrams
+# Unigrams
+frequencyReal, termsReal = calculateTermFrequency(realArticles);
+frequencyFake, termsFake = calculateTermFrequency(fakeArticles);
 
-# tokenize the document - this converts to lowercase and removes punctuation. Do this for all fake and all real articles
-fakeWords = list();
-print("cleaning fake")
-for article in fakeArticles:
-    fakeWords.extend(cleanText(article[1]));
+# Vocab sizes
+vocabReal = len(termsReal);
+vocabFake = len(termsFake);
 
-realWords = list();
-print("cleaning real")
-for article in realArticles:
-    realWords.extend(text_to_word_sequence(article[1]));
+# Total count of the frequencies
+totalFrequenciesReal = np.sum(np.sum(frequencyReal, axis=0));
+totalFrequenciesFake = np.sum(np.sum(frequencyFake, axis=0));
 
-print(realWords);
+#printFrequencyCount(frequency, terms);
+
+# Bayes rule for class c and document d - P(c|d) = P(d|c)P(c)/P(d)
+# We can get our class mapping by representing the document d as a set of features x1,x2...xn
+# And finding the class c s.t P(x1,x2...xn|c)P(c) is maximised
+# Hence calculate P(x1|c)P(x2|c)...P(xn|c) for real and fake classes for our documents
+
+
